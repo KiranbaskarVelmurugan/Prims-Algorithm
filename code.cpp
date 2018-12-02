dcaa48ba9fb64e1aa327d9cd5a13ad327329f5f3
KiranbaskarVelmurugan <kiranbaskar.v@rnpodarschool.com>
2018-12-02 13:28:43 +0530

    Add an addBefore function to add a node before a given node; Fix few errors in previous functions; Add appropriate length increment and decrements

--- code.cpp
+++ code.cpp
@@ -59,43 +59,63 @@ public:
 		}
 		else if(search->number == x && search->next == NULL) {
 			pushBack(x);
-			return 1;
 		}
 		else {
 			node *n = new node;
 			n->number = x;
-			node *next_element = n->next; //The next node to the one with x as a member in the original linked list
+			node *next_element = search->next; //The next node to the one with x as a member in the original linked list
 			next_element->prev = n;
 			search->next = n;
 			n->next = next_element;
 			n->prev = search;
-			return 1;
 		}
+		++length;
+		return 1;
 	}
 
-	int delElement(int x) {
+	addBefore(int x) {
+		node *search = front;
+		while(search->next != NULL && search->number != x);
+		if(search->number != x) {
+			cout<< "Node not found!";
+			return -1;
+		}
+		else if(search->number == x && search->prev == NULL) {
+			pushFront(x);
+		}
+		else {
+			node *n = new node;
+			n->number = x;
+			node *prev_element = search->prev; //The previous node to the one with x as a member in the original linked list
+			prev_element->next = n;
+			search->prev = n;
+			n->prev = prev_element;
+			n->next = search;
+		}
+		++length;
+		return 1;
+	}
+
+	int delNode(int x) {
 		if (front == NULL) {
 			cout<<"Underflow!";
 			return -1;
 		}
-
 		node *temp = front;
 		while(temp->next != NULL) {
 			if(temp->number == x) {
 				cout<<"Removing "<<x;
-				node *d = temp;
 				node *p = temp->prev;
 				node *n = temp->next;
 				p->next = n;
 				n->prev = p;
 				delete temp;
-				length--;
-				break;
-			}
-			else {
-				cout<<"Element "<< x <<" not found!";
+				--length;
+				return 1;
 			}
 		}
+		cout<<"Node not found!";
+		return -1;
 	} 
 };
 
